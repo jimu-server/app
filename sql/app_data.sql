fcae23ef7d8a94b0a03abc37ae75aabba0d19517
awen <zhiwen_der@qq.com>
2024-04-24 23:18:59 +0800

    添加删除模型,添加内置模型

--- sql/app_data.sql
+++ sql/app_data.sql
@@ -212,18 +212,27 @@ values (6, 1, 1, 1, 5);
 
 -- 初始化模型数据
 insert into app_chat_model(id, name, model, picture, size, is_download)
-values (1, 'Llama:7B', 'llama2', 'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fllama.png',
+values (1, 'Llama2:7B', 'llama2', 'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fllama.png',
         '3.8GB', true);
 
 insert into app_chat_model(id, name, model, picture, size, is_download)
-values (2, 'Llama:8B', 'llama3', 'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fllama.png',
+values (2, 'Code Llama2:7B', 'codellama:7b',
+        'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fllama.png',
+        '3.8GB', false);
+
+insert into app_chat_model(id, name, model, picture, size, is_download)
+values (3, 'Llama3:8B', 'llama3', 'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fllama.png',
         '4.7GB', true);
 
 
 insert into app_chat_model(id, name, model, picture, size, is_download)
-values (3, 'Gemma', 'gemma:2b', 'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fgemma.png',
+values (4, 'Gemma:2B', 'gemma:2b', 'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fgemma.png',
         '1.7GB', false);
 
 insert into app_chat_model(id, name, model, picture, size, is_download)
-values (4, 'Mistral:7B', 'mistral', 'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fmaistral.png',
-        '1.7GB', true);
+values (5, 'Gemma:7B', 'gemma:7b', 'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fgemma.png',
+        '4.8GB', false);
+
+insert into app_chat_model(id, name, model, picture, size, is_download)
+values (6, 'Mistral:7B', 'mistral', 'https://jimuos-1252940994.cos.ap-nanjing.myqcloud.com/llm%2Fiocn%2Fmaistral.png',
+        '4.1GB', true);
