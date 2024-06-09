46aada5e2380e861aa1088e0166cc06f457489ab
awen <zhiwen_der@qq.com>
2024-06-09 23:34:41 +0800

    update

--- sql/app_gpt.sql
+++ sql/app_gpt.sql
@@ -13,7 +13,11 @@ comment on table app_chat_plugin is 'gpt 插件表';
 comment on column app_chat_plugin.name is '插件名';
 comment on column app_chat_plugin.icon is '插件图标';
 comment on column app_chat_plugin.model is '插件模型';
+
 comment on column app_chat_plugin.status is '状态 0:未启用 1:启用';
 
 insert into app_chat_plugin(id, name, icon, model)
-VALUES (1, 'AI Assistant', '', 'llama3:latest')
+VALUES (1, 'AI Assistant', 'jimu-ChatGPT', 'llama3:latest');
+
+insert into app_chat_plugin(id, name, icon, model)
+VALUES (2, 'AI Test', 'jimu-ChatGPT', 'llama2:latest')
