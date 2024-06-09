5a8b2ae5d8d85c9569015dc808a747edf5f02eef
awen <zhiwen_der@qq.com>
2024-06-09 18:32:08 +0800

    添加gpt插件获取接口

---
+++ sql/app_gpt.sql
@@ -0,0 +1,19 @@
+drop table if exists app_chat_plugin;
+create table app_chat_plugin
+(
+    id          varchar(30) primary key,
+    name        varchar(100),
+    icon        varchar(100),
+    model       varchar(100),
+    status      boolean               default true,
+    create_time timestamp(0) not null default now()
+);
+
+comment on table app_chat_plugin is 'gpt 插件表';
+comment on column app_chat_plugin.name is '插件名';
+comment on column app_chat_plugin.icon is '插件图标';
+comment on column app_chat_plugin.model is '插件模型';
+comment on column app_chat_plugin.status is '状态 0:未启用 1:启用';
+
+insert into app_chat_plugin(id, name, icon, model)
+VALUES (1, 'AI Assistant', '', 'llama3:latest')
