4926f74a03af2e81b9760b51f3d86bd37fd4e96a
awen <zhiwen_der@qq.com>
2024-06-16 11:23:48 +0800

    修改知识库生成

--- sql/app_gpt.sql
+++ sql/app_gpt.sql
@@ -2,9 +2,11 @@ drop table if exists app_chat_plugin;
 create table app_chat_plugin
 (
     id          varchar(30) primary key,
-    name        varchar(100),
-    icon        varchar(100),
-    model       varchar(100),
+    name        varchar(100) not null,
+    icon        varchar(100)          default '',
+    model       varchar(100)          default '',
+    float_view  varchar(100)          default '',
+    props       json                  default '{}',
     status      boolean               default true,
     create_time timestamp(0) not null default now()
 );
@@ -13,11 +15,16 @@ comment on table app_chat_plugin is 'gpt 插件表';
 comment on column app_chat_plugin.name is '插件名';
 comment on column app_chat_plugin.icon is '插件图标';
 comment on column app_chat_plugin.model is '插件模型';
+comment on column app_chat_plugin.float_view is '插件悬浮菜单面板组件';
+comment on column app_chat_plugin.props is '插件悬浮菜单面板组件属性';
 
 comment on column app_chat_plugin.status is '状态 0:未启用 1:启用';
 
 insert into app_chat_plugin(id, name, icon, model)
-VALUES (1, 'AI Assistant', 'jimu-ChatGPT', 'llama3:latest');
+VALUES (1, 'AI 助手', 'jimu-ChatGPT', 'qwen2:7b');
 
-insert into app_chat_plugin(id, name, icon, model)
-VALUES (2, 'AI Test', 'jimu-ChatGPT', 'llama2:latest')
+insert into app_chat_plugin(id, name, icon, model,float_view)
+VALUES (2, '编程助手', 'jimu-code', 'llama3:latest','ProgrammingAssistantPanelView');
+
+insert into app_chat_plugin(id, name, icon, model, float_view)
+VALUES (3, '知识库', 'jimu-zhishi', 'qwen2:7b', 'KnowledgePanelView');
